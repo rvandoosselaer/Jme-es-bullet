14f42eadac2593059280535be812994604158738
rvandoosselaer <remy.vand@gmail.com>
2020-03-24 12:59:06 +0100

    renamed library to 'Jme-es-physics'

---
+++ demo/character/src/main/java/com/rvandoosselaer/jmeesphysics/character/Main.java
@@ -0,0 +1,251 @@
+package com.rvandoosselaer.jmeesphysics.character;
+
+import com.jme3.app.ChaseCameraAppState;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.StatsAppState;
+import com.jme3.bullet.collision.PhysicsCollisionObject;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.input.KeyInput;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
+import com.jme3.post.filters.FXAAFilter;
+import com.jme3.post.ssao.SSAOFilter;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.shadow.DirectionalLightShadowFilter;
+import com.jme3.shadow.EdgeFilteringMode;
+import com.rvandoosselaer.jmeesphysics.BulletSystem;
+import com.rvandoosselaer.jmeesphysics.CollisionShapeHelper;
+import com.rvandoosselaer.jmeesphysics.DefaultPhysicalShapeRegistry;
+import com.rvandoosselaer.jmeesphysics.PhysicalShapeRegistry;
+import com.rvandoosselaer.jmeesphysics.debug.BulletSystemDebugState;
+import com.rvandoosselaer.jmeesphysics.debug.PhysicalEntityDebugPublisher;
+import com.rvandoosselaer.jmeesphysics.es.Mass;
+import com.rvandoosselaer.jmeesphysics.es.PhysicalShape;
+import com.rvandoosselaer.jmeesphysics.es.WarpPosition;
+import com.simsilica.es.EntityData;
+import com.simsilica.es.EntityId;
+import com.simsilica.es.WatchedEntity;
+import com.simsilica.es.base.DefaultEntityData;
+import com.simsilica.lemur.GuiGlobals;
+import com.simsilica.lemur.input.AnalogFunctionListener;
+import com.simsilica.lemur.input.FunctionId;
+import com.simsilica.lemur.input.InputMapper;
+import com.simsilica.lemur.input.InputState;
+import com.simsilica.lemur.input.StateFunctionListener;
+import com.simsilica.lemur.style.BaseStyles;
+import com.simsilica.state.GameSystemsState;
+
+public class Main extends SimpleApplication implements StateFunctionListener, AnalogFunctionListener {
+
+    public static final FunctionId FUNCTION_MOVE = new FunctionId("move");
+    public static final FunctionId FUNCTION_STRAFE = new FunctionId("strafe");
+    public static final FunctionId FUNCTION_JUMP = new FunctionId("jump");
+
+    private GameSystemsState systems;
+    private EntityData entityData;
+    private BulletSystem bulletSystem;
+    private PhysicalShapeRegistry shapeRegistry;
+    private AmbientLight ambientLight;
+    private DirectionalLight directionalLight;
+    private ModelRegistry modelRegistry;
+    private EntityId playerId;
+    private WatchedEntity player;
+    private InputMapper inputMapper;
+    private Node cameraTarget = new Node();
+
+    private boolean setupComplete = false;
+    private boolean rotating = false;
+    private float yaw;
+    private float turnSpeed = 1;
+    private float moveSpeed = 1;
+    private float move;
+    private float strafe;
+    private boolean jumping;
+
+    public static void main(String[] args) {
+        new Main().start();
+    }
+
+    public Main() {
+        super(new StatsAppState(),
+                new GameSystemsState(true),
+                new ChaseCameraAppState());
+    }
+
+    @Override
+    public void simpleInitApp() {
+        GuiGlobals.initialize(this);
+        BaseStyles.loadGlassStyle();
+        GuiGlobals.getInstance().getStyles().setDefaultStyle("glass");
+
+        setupGameSystems();
+
+        setupLights();
+
+        setupPostProcessing();
+
+        inputMapper = GuiGlobals.getInstance().getInputMapper();
+        inputMapper.map(FUNCTION_MOVE, KeyInput.KEY_W);
+        inputMapper.map(FUNCTION_MOVE, KeyInput.KEY_UP);
+        inputMapper.map(FUNCTION_MOVE, InputState.Negative, KeyInput.KEY_S);
+        inputMapper.map(FUNCTION_MOVE, InputState.Negative, KeyInput.KEY_DOWN);
+        inputMapper.map(FUNCTION_STRAFE, InputState.Negative, KeyInput.KEY_A);
+        inputMapper.map(FUNCTION_STRAFE, KeyInput.KEY_D);
+        inputMapper.map(FUNCTION_JUMP, KeyInput.KEY_SPACE);
+
+        inputMapper.addStateListener(this, FUNCTION_JUMP);
+        inputMapper.addAnalogListener(this, FUNCTION_MOVE, FUNCTION_STRAFE);
+
+        ChaseCameraAppState chaseCameraAppState = getStateManager().getState(ChaseCameraAppState.class);
+        chaseCameraAppState.setDragToRotate(true);
+        chaseCameraAppState.setDefaultVerticalRotation(45 * FastMath.DEG_TO_RAD);
+        chaseCameraAppState.setTarget(cameraTarget);
+    }
+
+    protected void setupGameSystems() {
+        systems = getStateManager().getState(GameSystemsState.class);
+
+        // register global objects and systems
+        entityData = systems.register(EntityData.class, new DefaultEntityData());
+        shapeRegistry = systems.register(PhysicalShapeRegistry.class, new DefaultPhysicalShapeRegistry());
+        modelRegistry = systems.register(ModelRegistry.class, new DefaultModelRegistry());
+        bulletSystem = systems.register(BulletSystem.class, new BulletSystem());
+        getStateManager().attach(new BulletSystemDebugState(entityData, shapeRegistry));
+        getStateManager().attach(new VisualState(entityData, modelRegistry));
+        getStateManager().attach(new PlayerInputState(entityData, bulletSystem));
+
+        // load the level
+        Spatial level = getAssetManager().loadModel("Scenes/demo-level.blend.j3o");
+        level.depthFirstTraversal(visitor -> {
+            if (visitor instanceof Geometry) {
+                ColorRGBA diffuse = ((Geometry) visitor).getMaterial().getParamValue("Diffuse");
+                ((Geometry) visitor).getMaterial().setColor("Ambient", diffuse);
+            }
+        });
+        level.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+        rootNode.attachChild(level);
+
+        shapeRegistry.register(new PhysicalShape("level"), CollisionShapeHelper.createMeshShape(level));
+        entityData.setComponents(entityData.createEntity(), new Mass(0), new PhysicalShape("level"), new WarpPosition(level.getWorldTranslation(), level.getWorldRotation()));
+
+        // load the player
+        shapeRegistry.register(new PhysicalShape("player"), CollisionShapeHelper.createCapsuleShape(0.5f, 1.4f, true));
+        Spatial monkey = assetManager.loadModel("Models/Jaime/Jaime.j3o");
+        monkey.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+        modelRegistry.register(new Model("monkey"), monkey);
+
+        playerId = entityData.createEntity();
+        entityData.setComponents(playerId, new PlayerInput(new Vector3f(), new Quaternion(), false), new Mass(80f), new PhysicalShape("player"), new WarpPosition(new Vector3f(-5, 0.1f, 0), new Quaternion()), new Model("monkey"));
+
+        player = entityData.watchEntity(playerId, Position.class, Velocity.class);
+
+        // load another player
+        entityData.setComponents(entityData.createEntity(), new Model("monkey"), new Mass(80f), new PhysicalShape("player"), new WarpPosition(new Vector3f(5, 0.1f, 3), new Quaternion()));
+
+        // add bullet listeners
+        bulletSystem.addPhysicalEntityListener(new PositionPublisher(entityData));
+        bulletSystem.addPhysicalEntityListener(new PhysicalEntityDebugPublisher(entityData));
+    }
+
+    protected void setupLights() {
+        ambientLight = new AmbientLight(new ColorRGBA(0.3f, 0.3f, 0.3f, 1.0f));
+        directionalLight = new DirectionalLight(new Vector3f(-0.5f, -1.0f, -0.5f).normalizeLocal(), ColorRGBA.White);
+
+        rootNode.addLight(ambientLight);
+        rootNode.addLight(directionalLight);
+    }
+
+    protected void setupPostProcessing() {
+        FilterPostProcessor fpp = new FilterPostProcessor(getAssetManager());
+        getViewPort().addProcessor(fpp);
+
+        // check sampling
+        int samples = getContext().getSettings().getSamples();
+        boolean aa = samples != 0;
+        if (aa) {
+            fpp.setNumSamples(samples);
+        }
+
+        // shadow filter
+        DirectionalLightShadowFilter shadowFilter = new DirectionalLightShadowFilter(assetManager, 1024, 4);
+        shadowFilter.setLight(directionalLight);
+        shadowFilter.setEdgeFilteringMode(EdgeFilteringMode.PCFPOISSON);
+        shadowFilter.setEdgesThickness(2);
+        shadowFilter.setShadowIntensity(0.75f);
+        shadowFilter.setLambda(0.65f);
+        shadowFilter.setShadowZExtend(75);
+        shadowFilter.setEnabled(true);
+        fpp.addFilter(shadowFilter);
+
+        // SSAO
+        SSAOFilter ssaoFilter = new SSAOFilter();
+        ssaoFilter.setEnabled(false);
+        fpp.addFilter(ssaoFilter);
+
+        // setup FXAA if regular AA is off
+        if (!aa) {
+            FXAAFilter fxaaFilter = new FXAAFilter();
+            fxaaFilter.setEnabled(true);
+            fpp.addFilter(fxaaFilter);
+        }
+    }
+
+    @Override
+    public void valueChanged(FunctionId func, InputState value, double tpf) {
+        if (func == FUNCTION_JUMP) {
+            jumping = value != InputState.Off;
+        }
+    }
+
+    @Override
+    public void valueActive(FunctionId func, double value, double tpf) {
+        if (func == FUNCTION_MOVE) {
+            move = (float) value;
+        } else if (func == FUNCTION_STRAFE) {
+            strafe = (float) -value;
+        }
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        super.simpleUpdate(tpf);
+
+        if (bulletSystem.isInitialized() && !setupComplete) {
+            // set gravity
+            bulletSystem.getPhysicsSpace().setGravity(new Vector3f(0, -20f, 0));
+            // add filter to disable player collisions; return true if the collision should happen, false otherwise
+            bulletSystem.getPhysicsSpace().addCollisionGroupListener((nodeA, nodeB) -> {
+                // when both PhysicsCollisionObject have a the player's collisionshape, we ignore the collision
+                CollisionShape shape = shapeRegistry.get(new PhysicalShape("player"));
+                // when one of the collision objects isn't a player, the collision should happen
+                return !nodeA.getCollisionShape().equals(shape) || !nodeB.getCollisionShape().equals(shape);
+            }, PhysicsCollisionObject.COLLISION_GROUP_01);
+            setupComplete = true;
+        }
+
+        // input
+        Vector3f movement = new Vector3f(strafe * moveSpeed, 0, move * moveSpeed);
+
+        float[] angles = new float[3];
+        cam.getRotation().toAngles(angles);
+        Quaternion rotation = new Quaternion().fromAngles(0, angles[1], 0);
+
+        movement = rotation.mult(movement);
+
+        player.set(new PlayerInput(movement, rotation, jumping));
+        // this does exactly the same as watchedEntity.set()
+        // entityData.setComponent(playerId, new PlayerInput(new Vector3f(), new Quaternion().fromAngles(0, angles[1], 0)));
+
+        if (player.applyChanges()) {
+            cameraTarget.setLocalTranslation(player.get(Position.class).getLocation());
+        }
+    }
+}
---
+++ demo/character/src/main/java/com/rvandoosselaer/jmeesphysics/character/VisualState.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2020, rvandoosselaer
+ * All rights reserved.
+ * <p>
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * <p>
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * <p>
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * <p>
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rvandoosselaer.jmeesphysics.character;
+
+import com.jme3.anim.AnimComposer;
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.BaseAppState;
+import com.jme3.scene.Spatial;
+import com.simsilica.es.Entity;
+import com.simsilica.es.EntityContainer;
+import com.simsilica.es.EntityData;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class VisualState extends BaseAppState {
+
+    private final EntityData entityData;
+    private final ModelRegistry modelRegistry;
+
+    private ModelContainer models;
+
+    @Override
+    protected void initialize(Application app) {
+        models = new ModelContainer(entityData);
+        models.start();
+    }
+
+    @Override
+    protected void cleanup(Application app) {
+        models.stop();
+    }
+
+    @Override
+    public void update(float tpf) {
+        models.update();
+    }
+
+    private class ModelContainer extends EntityContainer<Spatial> {
+
+        public ModelContainer(EntityData ed) {
+            super(ed, Model.class, Position.class);
+        }
+
+        @Override
+        protected Spatial addObject(Entity e) {
+            Model model = e.get(Model.class);
+            Position position = e.get(Position.class);
+
+            Spatial spatial = modelRegistry.get(model).clone(false);
+            AnimComposer anim = spatial.getControl(AnimComposer.class);
+            if (anim != null) {
+                anim.setCurrentAction("Idle");
+            }
+            spatial.setLocalTranslation(position.getLocation());
+            spatial.setLocalRotation(position.getRotation());
+            ((SimpleApplication) getApplication()).getRootNode().attachChild(spatial);
+
+            return spatial;
+        }
+
+        @Override
+        protected void updateObject(Spatial object, Entity e) {
+            Position position = e.get(Position.class);
+
+            object.setLocalTranslation(position.getLocation());
+            object.setLocalRotation(position.getRotation());
+        }
+
+        @Override
+        protected void removeObject(Spatial object, Entity e) {
+            object.removeFromParent();
+        }
+
+    }
+
+    @Override
+    protected void onEnable() {
+    }
+
+    @Override
+    protected void onDisable() {
+    }
+
+}
